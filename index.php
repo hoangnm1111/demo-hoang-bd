07e0f38458a6d26f689c157be34b11e8b0c07aeb
Vuong Hoang <hoangasphalt89@gmail.com>
2023-10-12 19:31:07 +0700

    header design update

--- index.php
+++ index.php
@@ -46,8 +46,11 @@
             </ul>
             <div class="d-flex">
                 
-                <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+                <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
                     Đăng nhập
+                </button>
+                <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+                    Đăng ký
                 </button>
             </div>
             </div>
@@ -73,8 +76,39 @@
                             <label class="form-label">Mật khẩu</label>
                             <input type="password" class="form-control shadow-none" >
                         </div>
-                        <div>
-                            <button>ĐĂNG NHẬP</button>
+                        <div class="d-flex align-items-center justify-content-between ">
+                            <button type="submit" class="btn btn-dark shadow-none">ĐĂNG NHẬP</button>
+                            <a href= "javascipts: void(0)" class="text-secondary text-decoration-none">Quên mật khẩu?</a>
+                        </div>
+                    </div> 
+                </form>
+            
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <h5 class="modal-title d-flex align-items-center">
+                            <i class="bi bi-person-circle fs-3 me-2"></i> Người dùng đăng nhập
+                        </h5>
+                        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label">Địa chỉ Email</label>
+                            <input type="email" class="form-control shadow-none" >
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Mật khẩu</label>
+                            <input type="password" class="form-control shadow-none" >
+                        </div>
+                        <div class="d-flex align-items-center justify-content-between ">
+                            <button type="submit" class="btn btn-dark shadow-none">ĐĂNG NHẬP</button>
+                            <a href= "javascipts: void(0)" class="text-secondary text-decoration-none">Quên mật khẩu?</a>
                         </div>
                     </div> 
                 </form>
